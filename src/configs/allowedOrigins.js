9c07e0807f829207bda4e6caeb11bccf3f729662
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-26 12:28:59 -0300

    configs folder with cors dbconn and origins

---
+++ src/configs/allowedOrigins.js
@@ -0,0 +1,15 @@
+const allowedOrigins = [
+    'http://www.google.com', 
+    '127.0.0.1:5500', 
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'https://projeto14-mywallet-front-mocha.vercel.app',
+    'http://projeto14-mywallet-front-mocha.vercel.app',
+    'projeto14-mywallet-front-mocha.vercel.app',
+    'https://projeto14-mywallet-front-c2puajycv-thomasleick.vercel.app',
+    'http://projeto14-mywallet-front-c2puajycv-thomasleick.vercel.app',
+    'projeto14-mywallet-front-c2puajycv-thomasleick.vercel.app'
+    
+]
+
+module.exports = allowedOrigins
